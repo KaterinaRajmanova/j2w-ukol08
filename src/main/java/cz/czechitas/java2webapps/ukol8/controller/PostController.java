345c4b77f25585759917555449e372ddbfd5a97d
KaterinaRajmanova <valentova.kacka@seznam.cz>
2021-05-30 18:22:01 +0200

    splněn bod 8

---
+++ src/main/java/cz/czechitas/java2webapps/ukol8/controller/PostController.java
@@ -0,0 +1,27 @@
+package cz.czechitas.java2webapps.ukol8.controller;
+
+import cz.czechitas.java2webapps.ukol8.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PostController {
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/")
+    public ModelAndView seznam(){
+        ModelAndView seznamPost = new ModelAndView("seznam");
+        seznamPost.addObject("post", postService.list());
+        return seznamPost;
+    }
+
+
+
+}
